a880f231e3f1382e46cad925d1bebaccd671bda8
TRI THANH <trannguyentrithanh38@gmail.com>
2025-12-17 00:22:26 +0700

    chore: Finished all task in final project

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,23 +6,95 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  // Check user tồn tại
+  // Có thể dùng vòng lặp for để duyệt qua mảng users rồi check
+  // Ở đây dùng hàm .some() trong js cho nhanh
+  const doesExist = users.some(user => user.username === username);
+  return doesExist;
 }
 
+// Check tài khoản user 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  // Lọc validusers bằng hàm .filter()
+  const validusers = users.filter(user => {
+    return user.username === username && user.password === password;
+  });
+
+  // Nghĩa là tồn tại tài khoản này
+  return validusers.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy username và password từ body của URL
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password){
+    return res.status(404).json({message: "Error logging in"});
+  } 
+
+  if(authenticatedUser(username, password)){
+    // Nếu tài khoản đã đăng ký
+    // Tạo JWT Token
+    let accessToken = jwt.sign({
+      data : password
+    }, 'access', {expiresIn: 60 * 60});
+
+    // Lưu token và username vào session
+    req.session.authorization = {
+      accessToken, username
+    }
+    return res.status(200).json("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"})
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy nội dung review sau query (?) và gắn nó với username đang đăng nhập trong session
+
+  // Lấy isbn từ URL
+  const isbn = req.params.isbn;
+
+  // 
+  const review = req.query.review;
+
+  // Lấy username từ session để đảm bảo đúng người đăng nhập
+  const username = req.session.authorization['username'];
+
+  if(books[isbn]){
+    // Nếu book tồn tại
+    // reviews trong book là 1 object
+    // Ở đây ta sẽ thêm nội dung review vào với key là username (người đưa ra review)
+    // Nếu user này chưa review -> Tạo review mới
+    // Nếu user này đã review -> Update lại review cũ
+    
+    books[isbn].reviews[username] = review;
+    return res.status(200).send(`The review for the book with ISBN ${isbn} has been added/updated.`);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization['username'];
+
+  if(books[isbn]){
+    // Check xem user đã có review cuốn sách này chưa
+    if(books[isbn].reviews[username]){
+      // Dùng lệnh delete để xóa thuộc tính trong object
+      delete books[isbn].reviews[username];
+      return res.status(200).send(`Reviews for the ISBN ${isbn} posted by the user ${username} deleted.`);
+    } else {
+        return res.status(404).json({message: "Review not found for this user"});
+    }
+  } else {
+      return res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,169 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy username và password từ body của req
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(username && password){ 
+
+    if(isValid){
+      res.status(404).json({message: "User already exists"});
+    } else {
+      // Nếu user chưa tồn tại thì thêm vào mảng và thông báo đăng ký thành công
+      users.push({"username" : username, "password" : password});
+      res.status(200).json({message : "User successfully registered. Now you can login"})
+    }
+  }
+  // Nếu thiếu username hoặc password
+  return res.status(404).json({message: "Unable to register user."});
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Trả về danh sách tất cả sách dưới dạng string (đã được định dạng cho đẹp)
+  // Thụt đầu dòng 4 ô
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy giá trị isbn từ tham số (params) của URL
+  const isbn = req.params.isbn;
+
+  // Check nếu book tồn tại thì trả về, không thì gửi tin nhắn
+  if(books[isbn]){
+    return res.status(200).json(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy giá trị author từ params của URL
+  const authorName = req.params.author;
+
+  // Books là object, cần tạo mảng các book có cùng tác giả
+  const matchingBooks = [];
+
+  // Lấy danh sách key trong books và duyệt từng key để kiểm tra author
+  const keys = Object.keys(books);
+  keys.forEach(key => {
+    if(books[key].author === authorName){
+      matchingBooks.push(books[key]);
+    }
+  })
+  
+  // Trả về
+  if(matchingBooks.length > 0){
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({message: "Author not found"});
+  }
+  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy giá trị title từ params của URL
+  const title = req.params.title;
+
+  // Books là object, cần tạo mảng các book có cùng tác giả
+  const matchingBooks = [];
+
+  // Lấy danh sách key trong books và duyệt từng key để kiểm tra author
+  const keys = Object.keys(books);
+  keys.forEach(key => {
+    if(books[key].title === title){
+      matchingBooks.push(books[key]);
+    }
+  })
+  
+  // Trả về
+  if(matchingBooks.length > 0){
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({message: "Title not found"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Lấy giá trị isbn từ params của URL
+  const isbn = req.params.isbn;
+
+  if(books[isbn]){
+    return res.status(200).json(books[isbn].review)
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+
 });
+
+// Task 10: 
+// Add the code for getting the list of books available in the shop (done in Task 1) 
+// using Promise callbacks or async-await with Axios.
+
+const getAllBooks = async () => {
+  try{
+    // Gọi đường dẫn lấy tất cả sách
+    const response = await axios.get("http://localhost:5000/");
+    console.log(response.data);
+  } catch{
+    console.error("Error fetching books:", error);
+  }
+}
+
+// Task 11: 
+// Add the code for getting the book details based on ISBN (done in Task 2)
+// using Promise callbacks or async-await with Axios.
+
+const getBookOnISBN = async (isbn) => {
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching book by ISBN:", error.message);
+  }
+}
+
+// Task 12: 
+// Add the code for getting the book details based on Author (done in Task 3)
+// using Promise callbacks or async-await with Axios.
+
+const getBookOnAuthor = async (author) => {
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching book by Author:", error.message);
+  }
+}
+
+// Task 13: 
+// Add the code for getting the book details based on Title (done in Task 4)
+// using Promise callbacks or async-await with Axios.
+
+const getBookOnTitle = async (title) => {
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching book by Title:", error.message);
+  }
+}
+
+
+
+
+
+
 
 module.exports.general = public_users;
